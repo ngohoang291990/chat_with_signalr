84c3f6f32a1ba4162843933ffe8c761ee2b95f4d
ngohoang29 <ngohoang29@gmail.com>
2019-12-19 20:54:34 +0700

    Commit Inital

---
+++ Hubs/DemoChat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace Demochat.Hubs
+{
+    [HubName("chat")]
+    public class DemoChat : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public void Connect(string name)
+        {
+            Clients.Caller.connect(name);
+        }
+        public void Message(string name,string message)
+        {
+            Clients.All.message(name,message);
+        }
+    }
+}
